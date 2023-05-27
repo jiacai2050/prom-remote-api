99ec517616cb833032543f42ff4c778d8c5ecca3
Jiacai Liu <dev@liujiacai.net>
2023-05-27 19:54:35 +0800

    feat: support actix web framework (#3)
    * feat: support actix web framework
    
    * update template

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,5 +1,7 @@
 //! This module provides various middlewares/handlers for popular web framework to
 //! help implementing write/read procotols of remote storage.
 
+#[cfg(feature = "actix")]
+pub mod actix;
 #[cfg(feature = "warp")]
 pub mod warp;
